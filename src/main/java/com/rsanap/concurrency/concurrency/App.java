2c4aa719659da39e89a5a438f60674cf6f272ca6
rameshsanap <rsanap.gmail.com>
2015-12-13 13:00:00 +0530

    Added shutdown hook to executor

--- src/main/java/com/rsanap/concurrency/concurrency/App.java
+++ src/main/java/com/rsanap/concurrency/concurrency/App.java
@@ -18,13 +18,20 @@ public class App {
 					for (int i = 1; i < 2; i++)
 						synchronized (count) {
 							System.out.println(Thread.currentThread().getName()
-									+ " count " + count++);	
+									+ " count " + count++);
 						}
-					
+					/*System.out.println("Thread exiting "
+							+ Thread.currentThread().getName());*/
 				}
 			};
 			executors.submit(runnable);
+			// System.out.println("Exiting now!!");
 		}
+		// Initiates orderly shutdown, executes already submitted tasks.
+		// Executor will not accept new tasks
+
+		executors.shutdown();
+		
 	}
 
 	private static ExecutorService executors = Executors.newFixedThreadPool(5);
